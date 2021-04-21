93ee50db630f061332afe38130df54e51ceb23e1
smasher164 <aindurti@gmail.com>
2021-04-20 22:48:14 -0400

    don't need loadLibrary

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
     private static native void print();
 
     public static void main(String[] args) {
-        System.loadLibrary("Native");
 		HelloWorld.print();
     }
 
